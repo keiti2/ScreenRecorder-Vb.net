12a41d07fef738f806a61163bc07349028a5fd28
Henrique Keiti <henrique.keiti2@gmail.com>
2020-06-12 12:16:08 -0300

    Resolucao full hd e caminho fixo C:

---
+++ WindowsApp3/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports Accord.Video.FFMPEG
+
+Public Class Form1
+
+    Private vf As VideoFileWriter
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        vf = New VideoFileWriter()
+        vf.Open("C:\winfood\video\Exported_Video23.avi", 1920, 1080, 10, VideoCodec.MPEG4, 3000000)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim bp = New Bitmap(1920, 1080)
+        Dim gr = Graphics.FromImage(bp)
+        gr.CopyFromScreen(0, 0, 0, 0, New Size(bp.Width, bp.Height))
+        pictureBox1.Image = bp
+        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        vf.WriteVideoFrame(bp)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Stop()
+
+        vf.Close()
+    End Sub
+End Class
